ac5f767e223b621e9564fc6570140eb3279c7846
Maksym Melnychok <maksym.melnychok@gmail.com>
2015-03-23 16:00:33 +0100

    Squash merge of garethr:support-vpc-default-security-groups

--- lib/puppet/type/ec2_securitygroup.rb
+++ lib/puppet/type/ec2_securitygroup.rb
@@ -6,11 +6,17 @@ Puppet::Type.newtype(:ec2_securitygroup) do
 
   ensurable
 
-  newparam(:name, namevar: true) do
-    desc 'the name of the security group'
+  newparam(:name) do
+    desc 'the name of the security group resource'
+    isnamevar
     validate do |value|
       fail 'Security groups must have a name' if value == ''
     end
+  end
+
+  newparam(:group_name) do
+    desc 'the name of the security group'
+    isnamevar
   end
 
   newproperty(:region) do
@@ -60,6 +66,7 @@ Puppet::Type.newtype(:ec2_securitygroup) do
 
   newproperty(:vpc) do
     desc 'A VPC to which the group should be associated'
+    isnamevar
   end
 
   def should_autorequire?(rule)
@@ -77,4 +84,25 @@ Puppet::Type.newtype(:ec2_securitygroup) do
   autorequire(:ec2_vpc) do
     self[:vpc]
   end
+
+  # When you create a VPC you automatically get a security group called default. You can't change the name.
+  # This lack of uniqueness makes managing these default security groups difficult. Enter a composite namevar.
+  # We support two name formats:
+  #
+  #   1. {some-security-group}
+  #   2. {some-vpc-name}::default
+  #
+  # Note that we only support prefixing a security group name with the vpc name for the default security group
+  # at this point. This avoids the issue of otherwise needing to store the resources in two places for non-default
+  # VPC secueity groups.
+  #
+  # In the case of a a default security group, we maintain the full name (including the VPC name) in the name property
+  # as otherwise it won't be unique and uniqueness and composite namevars are fun.
+  def self.title_patterns
+    [
+      [ /^(([\w\-]+)::(default))$/, [ [ :name, lambda {|x| x} ], [ :vpc, lambda {|x| x} ], [ :group_name, lambda {|x| x} ] ] ],
+      [ /^(([\w\-]+))$/, [ [ :name, lambda {|x| x} ], [ :group_name, lambda {|x| x} ] ] ]
+    ]
+  end
+
 end
