4dfbd1fb872c25b0a957dc617be7878b0e293fec
keymone <keymone@gmail.com>
2015-07-12 17:46:59 +0200

    stub launchconfig spec

--- spec/unit/provider/ec2_launchconfig/v2_spec.rb
+++ spec/unit/provider/ec2_launchconfig/v2_spec.rb
@@ -18,38 +18,53 @@ describe provider_class do
 
   let(:instance) { provider.class.instances.first }
 
+  let(:client) { provider.autoscaling_client }
+
+  before(:each) { stub_autoscaling }
+
   it 'should be an instance of the ProviderV2' do
     expect(provider).to be_an_instance_of Puppet::Type::Ec2_launchconfiguration::ProviderV2
   end
 
   describe 'self.prefetch' do
     it 'should exist' do
+      client.expects(:describe_launch_configurations).returns([]).twice
       provider.class.instances
       provider.class.prefetch({})
     end
   end
 
-  context 'with the minimum params' do
-    describe 'running create' do
-      it 'should send a request to the EC2 API to create the launch configuration' do
-        expect(provider.create).to be_truthy
-      end
+  describe 'running create' do
+    it 'should request to create launch configuration' do
+      client.expects(:create_launch_configuration).returns(true)
+      expect(provider.create).to be_truthy
+    end
+  end
+
+  describe 'running exists?' do
+    it 'falsy' do
+      expect(provider.exists?).to be_falsy
     end
 
-    describe 'running exists?' do
-      it 'should correctly report non-existent instances' do
-        expect(provider.exists?).to be_falsy
-      end
-
-      it 'should correctly find existing launch configurations' do
-        expect(instance.exists?).to be_truthy
-      end
+    it 'truthy' do
+      client.expects(:describe_launch_configurations).returns(
+        [stub(data: stub(launch_configurations: [stub(
+          security_groups: [],
+          launch_configuration_name: stub,
+          instance_type: stub,
+          image_id: stub,
+          key_name: stub,
+        )]))])
+      stub_ec2.expects(:describe_security_groups).returns(
+        stub(data: stub(security_groups: [])))
+      expect(instance.exists?).to be_truthy
     end
+  end
 
-    describe 'running destroy' do
-      it 'should send a request to the EC2 API to destroy the launch configuration' do
-        expect(provider.destroy).to be_truthy
-      end
+  describe 'running destroy' do
+    it 'should send request to destroy launch configuration' do
+      client.expects(:delete_launch_configuration).returns(true)
+      expect(provider.destroy).to be_truthy
     end
   end
 end
