db17c708631a72f82c46b9c871116bac866f8930
keymone <keymone@gmail.com>
2015-07-13 10:23:36 +0200

    stub route53 record spec

--- spec/unit/provider/route53_record/v2_spec.rb
+++ spec/unit/provider/route53_record/v2_spec.rb
@@ -3,55 +3,73 @@ require 'spec_helper'
 provider_class = Puppet::Type.type(:route53_a_record).provider(:v2)
 
 describe provider_class do
-  context 'with the minimum params' do
-    def domain; "notsotrivialexample.com."; end
-    def zone; @zone ||= Puppet::Type.type(:route53_zone).new(name: domain); end
+  def domain; "notsotrivialexample.com."; end
+  def zone; @zone ||= Puppet::Type.type(:route53_zone).new(name: domain); end
 
-    before(:all) { zone.provider.create }
-    after(:all) { zone.provider.destroy }
+  let(:resource) { Puppet::Type.type(:route53_a_record).new(
+    name: "local.#{domain}",
+    zone: domain,
+    ttl: 3000,
+    values: ['127.0.0.1']
+  )}
+  let(:provider) { resource.provider }
+  let(:instance) { provider.class.instances.first }
+  let(:route53) { provider.route53_client }
 
-    let(:resource) { Puppet::Type.type(:route53_a_record).new(
-      name: "local.#{domain}",
-      zone: domain,
-      ttl: 3000,
-      values: ['127.0.0.1']
-    )}
+  before(:each) { stub_route53 }
+  # before(:all) { zone.provider.create }
+  # after(:all) { zone.provider.destroy }
 
-    let(:provider) { resource.provider }
+  it 'should be an instance of the ProviderV2' do
+    expect(provider).to be_an_instance_of Puppet::Type::Route53_a_record::ProviderV2
+  end
 
-    let(:instance) { provider.class.instances.first }
+  describe '.prefetch' do
+    it 'exists' do
+      expect_hosted_zone.twice
+      route53.expects(:list_resource_record_sets).returns(stub(data: stub(
+        resource_record_sets: []))).twice
+      provider.class.instances
+      provider.class.prefetch({})
+    end
+  end
 
-    it 'should be an instance of the ProviderV2' do
-      expect(provider).to be_an_instance_of Puppet::Type::Route53_a_record::ProviderV2
+  describe '#create' do
+    it 'should send a request to the EC2 API to create the record' do
+      expect_hosted_zone
+      route53.expects(:change_resource_record_sets).returns(true)
+      expect(provider.create).to be_truthy
+    end
+  end
+
+  describe '#exists?' do
+    it 'should correctly report non-existent records' do
+      expect(provider.exists?).to be_falsy
     end
 
-    describe 'self.prefetch' do
-      it 'exists' do
-        provider.class.instances
-        provider.class.prefetch({})
-      end
+    it 'should correctly find existing records' do
+      expect_hosted_zone
+      route53.expects(:list_resource_record_sets).returns(stub(data: stub(
+        resource_record_sets: [stub('record',
+          type: 'A',
+          name: domain,
+          resource_records: [],
+          ttl: stub)])))
+      expect(instance.exists?).to be_truthy
     end
+  end
 
-    describe 'create' do
-      it 'should send a request to the EC2 API to create the record' do
-        expect(provider.create).to be_truthy
-      end
+  describe '#destroy' do
+    it 'should send a request to the EC2 API to destroy the record' do
+      expect_hosted_zone
+      route53.expects(:change_resource_record_sets).returns(true)
+      expect(provider.destroy).to be_truthy
     end
+  end
 
-    describe 'exists?' do
-      it 'should correctly report non-existent records' do
-        expect(provider.exists?).to be_falsy
-      end
-
-      it 'should correctly find existing records' do
-        expect(instance.exists?).to be_truthy
-      end
-    end
-
-    describe 'destroy' do
-      it 'should send a request to the EC2 API to destroy the record' do
-        expect(provider.destroy).to be_truthy
-      end
-    end
+  def expect_hosted_zone
+    route53.expects(:list_hosted_zones).returns(stub(data: stub(hosted_zones: [stub(
+      name: domain,
+      id: stub)])))
   end
 end
